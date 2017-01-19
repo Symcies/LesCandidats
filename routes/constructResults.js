3bf9c1594409a46c894b51ffb22f87a2511b7a18
Symcies <koval.igor.92@gmail.com>
2017-01-20 00:56:36 +0100

    processing of the answer checking query

--- routes/constructResults.js
+++ routes/constructResults.js
@@ -1,15 +1,14 @@
-//////////////////////////////////
-/// Different libraries loaded ///
-//////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+/// Different libraries loaded
+////////////////////////////////////////////////////////////////////////////////
 var async = require('async');
 
 
-///////////////////////////////
-/// Connect to MongoDB host ///
-///////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+/// Connect to MongoDB host
+////////////////////////////////////////////////////////////////////////////////
 var MongoClient = require('mongodb').MongoClient;
 var db;
-
 var MongoDBKey = require('./MongoDBKey');
 
 MongoClient.connect(MongoDBKey.key, (err, database) => {
@@ -17,29 +16,67 @@ MongoClient.connect(MongoDBKey.key, (err, database) => {
   db = database
 })
 
-
-
 ////////////
 // TODO : The previous MongoClient has to be used only once, between this file and userPreference.js
 ////////////
 
 
+////////////////////////////////////////////////////////////////////////////////
+/// Inspect and process the result of the database query
+////////////////////////////////////////////////////////////////////////////////
+
+var processQuery = function(questionChoices, questionAnswer) {
+  var userAnswer = {};
+  var topAnswer = 0;
+
+  var N = questionChoices.length;
+  for(var i = 0; i < N; ++i) {
+    choice = questionChoices[i];
+    Answers = choice['answers']
+    if(choice["value"] == questionAnswer)
+    {
+      userAnswer = Answers;
+    }
+
+    for(party in Answers) {
+      if(Answers.hasOwnProperty(party)) {
+        topAnswer = Math.max(topAnswer, Answers[party]);
+      }
+    }
+  }
+  return [userAnswer, topAnswer];
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/// Query the database
+////////////////////////////////////////////////////////////////////////////////
+
 var getResultsOfGivenQuestion = function(questionText, questionAnswer, callback) {
   var Query = {
     "question.name": questionText,
-    "question.choices" : { $elemMatch : { "value" : questionAnswer }}
   };
   var Projection = {
-    "question.choices.$": 1,
-    "_id": 0
+    "_id": 0,
+    "theme": 1,
+    "question.choices": 1,
   };
 
-  db.collection('questions').find(Query, Projection).toArray(function(err, politicalPreference) {
-    //console.log(politicalPreference[0].question.choices[0].answers);
-    callback(err, politicalPreference[0].question.choices[0].answers)
+  db.collection('questions').find(Query, Projection).toArray(function(err, queryResult) {
+
+    Answers = processQuery(queryResult[0]['question']['choices'], questionAnswer);
+
+    results = {};
+    results['userAnswer'] = Answers[0];
+    results['theme'] = queryResult[0]['theme'];
+    results['topAnswer'] = Answers[1];
+    callback(err, results)
   });
 
 };
+
+////////////////////////////////////////////////////////////////////////////////
+/// Connect to MongoDB host
+////////////////////////////////////////////////////////////////////////////////
 
 var sumJSObjects = function(finalObject, result) {
   for(key in result) {
@@ -51,41 +88,40 @@ var sumJSObjects = function(finalObject, result) {
   };
 };
 
+////////////////////////////////////////////////////////////////////////////////
+/// Connect to MongoDB host
+////////////////////////////////////////////////////////////////////////////////
 
-
-var computeResults = function(userSurveyResults, renderResults)
+var computeResults = function(surveyResults, renderResults)
 {
-  var iteratorResults = Object.keys(userSurveyResults);
-
+  var iteratorResults = Object.keys(surveyResults);
   getResultsPerQuestion = [];
   for(var i = 0; i < iteratorResults.length; ++i) {
     var questionText = iteratorResults[i];
-    var questionAnswer = userSurveyResults[questionText];
+    var questionAnswer = surveyResults[questionText];
     getResultsPerQuestion.push( getResultsOfGivenQuestion.bind(null, questionText, questionAnswer));
   };
 
+  async.parallel(
+    getResultsPerQuestion,
+    function(err, results) {
+      var finalSum = {};
+      var topAnswersSum = 0;
 
-
-  async.parallel( getResultsPerQuestion
-  , function(err, results) {
-    var finalSum = {};
-    var obj = null;
-    for(var i=0; i < results.length; i++) {
-      obj=results[i];
-      sumJSObjects(finalSum, obj);
-    }
-    renderResults(finalSum);
+      for(var i=0; i < results.length; i++) {
+        result = results[i];
+        topAnswersSum += result['topAnswer'];
+        sumJSObjects(finalSum, result['userAnswer']);
+      }
+      renderResults(finalSum);
   });
-
 
 };
 
+////////////////////////////////////////////////////////////////////////////////
+/// Exports function
+////////////////////////////////////////////////////////////////////////////////
 
-
-
-////////////////////////
-/// Export functions ///
-////////////////////////
 module.exports = {
   computeResults: computeResults,
 };
